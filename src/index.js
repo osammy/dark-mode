00199c0b5e1835f09dd5509aefa076944ced56f2
samuel <osamaimafidon@gmail.com>
2019-10-02 14:19:22 +0100

    one stretch attempted

--- src/index.js
+++ src/index.js
@@ -4,23 +4,53 @@ import axios from "axios";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
+import SearchByCoins from "./components/SearchByCoins";
 
 import "./styles.scss";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [coinList, setCoinList] = useState([]);
+  const [searchCoin, setSearchCoin] = useState("");
 
   useEffect(() => {
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
+      .then(res => {console.log(res.data);setCoinData(res.data)})
+      .catch(err => console.log(err));
+
+    axios
+      .get("https://api.coingecko.com/api/v3/coins/list")
+      .then(res => setCoinList(res.data))
       .catch(err => console.log(err));
   }, []);
+
+  const handleChange = e => {
+    setSearchCoin(e.target.value);
+  };
+
+  const handleSearch = () => {
+    //implement this later./coins/{id}/market_chart/range
+
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/${searchCoin}/
+        market_chart/range?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true`
+      )
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className="App">
       <Navbar />
+      <SearchByCoins
+        coins={coinList}
+        handleSearch={handleSearch}
+        handleChange={handleChange}
+      />
       <Charts coinData={coinData} />
     </div>
   );
