3607e0e1d5348c66a0c43df4949e5ce723c104fa
samuel <osamaimafidon@gmail.com>
2019-10-02 16:17:47 +0100

    uncommented axios side effects in index.js

--- src/index.js
+++ src/index.js
@@ -14,19 +14,19 @@ const App = () => {
   const [coinList, setCoinList] = useState([]);
   const [searchCoin, setSearchCoin] = useState("");
 
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-  //     )
-  //     .then(res => {console.log(res.data);setCoinData(res.data)})
-  //     .catch(err => console.log(err));
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
+      )
+      .then(res => {console.log(res.data);setCoinData(res.data)})
+      .catch(err => console.log(err));
 
-  //   axios
-  //     .get("https://api.coingecko.com/api/v3/coins/list")
-  //     .then(res => setCoinList(res.data))
-  //     .catch(err => console.log(err));
-  // }, []);
+    axios
+      .get("https://api.coingecko.com/api/v3/coins/list")
+      .then(res => setCoinList(res.data))
+      .catch(err => console.log(err));
+  }, []);
 
   const handleChange = e => {
     setSearchCoin(e.target.value);
